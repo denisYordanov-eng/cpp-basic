a476ad43b3ec6b7cd15f7e8c10ea20a86320668a
denisYordanov-eng <denisivov437@gmail.com>
2025-07-17 22:49:04 +0300

    Made my second task in C++

---
+++ 02-Numbersfrom1To10/02-Numbersfrom1To10.cpp
@@ -0,0 +1,14 @@
+// 02-Numbersfrom1To10.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << i << endl;
+
+	}
+}
