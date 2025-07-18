c53fb3fd70bf1ff15bdc7ce43bad9c5d80cce3c9
denisYordanov-eng <denisivov437@gmail.com>
2025-07-18 07:29:53 +0300

    Made a task.

---
+++ 04-InchToMM/04-InchToMM.cpp
@@ -0,0 +1,17 @@
+// 04-InchToMM.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using  namespace std;
+using std::cin;
+
+
+int main()
+{
+    double sm;
+    double inch;
+
+    cin >> sm;
+    inch = sm * 2.54;
+    cout << inch;
+}
